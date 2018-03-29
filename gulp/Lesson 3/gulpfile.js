a11bd1fdaf0a1b27dc736069ff4ff8bac8dd201c
Tobias Leinss <Caruso33@web.de>
2018-03-29 17:52:44 +0700

    finish gulp, very last lesson still bug

---
+++ gulp/Lesson 3/gulpfile.js
@@ -0,0 +1,30 @@
+var gulp = require('gulp');
+var sass = require('gulp-sass');
+var autoprefixer = require('gulp-autoprefixer');
+var browserSync = require('browser-sync').create();
+
+gulp.task('styles', done => {
+  gulp
+    .src('sass/**/*.scss')
+    .pipe(sass().on('error', sass.logError))
+    .pipe(
+      autoprefixer({
+        browsers: ['last 2 versions']
+      })
+    )
+    .pipe(gulp.dest('./css'))
+    .pipe(browserSync.stream());
+  done();
+});
+
+gulp.task(
+  'default',
+  gulp.series('styles', done => {
+    gulp.watch('sass/**/*.scss', gulp.task('styles'));
+
+    browserSync.init({
+      server: './'
+    });
+    browserSync.stream();
+  })
+);
